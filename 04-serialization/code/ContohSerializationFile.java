2b25eca7c8f4a178610f42e91a4cf52257b9e48a
tamami <tamami.oka@gmail.com>
2017-02-22 15:59:20 +0700

    init commit

---
+++ 04-serialization/code/ContohSerializationFile.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class ContohSerializationFile {
+	public static void main(String args[]) throws Exception {
+		Mahasiswa mhs = new Mahasiswa("0001","tamami");
+
+		FileOutputStream fout = new FileOutputStream("file.txt");
+		ObjectOutputStream out = new ObjectOutputStream(fout);
+
+		out.writeObject(mhs);
+		out.flush();
+		System.out.println("Selesai");
+	}
+
+	static class Mahasiswa implements Serializable {
+		private String nim;
+		private String nama;
+
+		public Mahasiswa(String nim, String nama) {
+			this.nim = nim;
+			this.nama = nama;
+		}
+	}
+}
