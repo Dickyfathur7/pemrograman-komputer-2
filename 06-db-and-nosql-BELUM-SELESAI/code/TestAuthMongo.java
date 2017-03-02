ffea03bb3d156e23ccaaabe45e62fca10fe8c911
tamami <tamami.oka@gmail.com>
2017-03-02 14:46:48 +0700

    finishing modul 06-db-and-nosql

--- 06-db-and-nosql-BELUM-SELESAI/code/TestAuthMongo.java
+++ 06-db-and-nosql-BELUM-SELESAI/code/TestAuthMongo.java
@@ -3,6 +3,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.DB;
 import org.bson.Document;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class TestAuthMongo {
 	public static void main(String args[]) {
@@ -10,20 +11,27 @@ public class TestAuthMongo {
 		MongoClient mongoClient = new MongoClient(uri);
 		System.out.println("Sukses terhubung dengan auth");
 
-		DB db = mongoClient.getDB("phb");
+		MongoDatabase db = mongoClient.getDatabase("phb");
 
 		MongoCollection<Document> coll = db.getCollection("mahasiswa");
 
 		Document document = new Document("nim", "17005");
 		document.append("name", "bambang");
-		document.insertOne(document);
+		coll.insertOne(document);
 		System.out.println("Data sudah masuk");
 
-		//=============================================
+		//============================================= tanpa auth
 
 		MongoClient mongoClient2 = new MongoClient("localhost", 27017);
 		System.out.println("Sukses terhubung tanpa auth");
 
-		DB db1 = mongoClient2.getDB("phb");
+		MongoDatabase db1 = mongoClient2.getDatabase("phb");
+
+		MongoCollection<Document> coll1 = db.getCollection("mahasiswa");
+
+		Document doc1 = new Document("nim", "17006");
+		doc1.append("name", "diva");
+		coll1.insertOne(doc1);
+		System.out.println("Data sudah masuk tanpa auth");
 	}
 }
