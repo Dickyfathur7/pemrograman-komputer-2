2ad2e9807b9bf12909bfebc974f5c7f900dc9aa4
tamami <tamami.oka@gmail.com>
2017-03-03 22:57:40 +0700

    mp3 player dengan javafx

--- 08-multimedia-BELUM-SELESAI/code/Mp3Player.java
+++ 08-multimedia-BELUM-SELESAI/code/Mp3Player.java
@@ -1,56 +1,56 @@
-import javax.media.Player;
-import javax.media.NoPlayerException;
-import javax.media.CannotRealizeException;
-import javax.media.Manager;
-import java.io.IOException;
-import java.io.File;
-import java.net.URL;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.Media;
 
-public class Mp3Player {
+public class Mp3Player extends Application {
 
-	private Player audioPlayer = null;
-
-	public Mp3Player(URL url) throws IOException, NoPlayerException, CannotRealizeException {
-		audioPlayer = Manager.createRealizedPlayer(url);
-	}
-
-	public Mp3Player(File file) throws IOException, NoPlayerException, CannotRealizeException {
-		this(file.toURL());
-	}
-
-	public void play() {
-		audioPlayer.start();
-	}
-
-	public void stop() {
-		audioPlayer.stop();
-		audioPlayer.close();
-	}
-
-	public static void cetakPetunjuk() {
-		System.out.println("Caranya: java Mp3Player <file-audio>");
-	}
+	static String namaFile;
+	boolean isPlay;
 
 	public static void main(String args[]) {
-		try {
-			if(args.length == 1) {
-				File audioFile = new File(args[0]);
-				Mp3Player player = new Mp3Player(audioFile);
-
-				System.out.println("Memutar file '" + audioFile.getAbsolutePath() + "'");
-				System.out.println("Tekan Enter untuk keluar");
-				player.play();
-
-				System.in.read();
-				System.out.println("Keluar");
-				player.stop();
-			} else {
-				cetakPetunjuk();
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		System.exit(0);
+		namaFile = args[0];
+		launch(args);
 	}
-	
+
+	@Override
+	public void start(Stage primaryStage) {
+		Media media = new Media(namaFile);
+		final MediaPlayer mediaPlayer = new MediaPlayer(media);
+		isPlay = false;
+
+		final Button btn = new Button();
+		btn.setText("Mainkan");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					if(!isPlay) {
+						mediaPlayer.play();
+						btn.setText("Berhenti");
+						isPlay = true;
+					} else {
+						mediaPlayer.stop();
+						btn.setText("Mulai");
+						isPlay = false;
+					}
+				} catch(Exception e) { e.printStackTrace(); }
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.getChildren().addAll(btn);
+
+		Scene scene = new Scene(root, 350, 250);
+
+		primaryStage.setTitle("Putar Mp3");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 }
