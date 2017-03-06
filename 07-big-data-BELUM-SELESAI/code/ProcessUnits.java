21e2f58feffb7c88b477ea8bfc9dadae3a5921e5
tamami <tamami.oka@gmail.com>
2017-03-06 08:39:29 +0700

    update materi big data

---
+++ 07-big-data-BELUM-SELESAI/code/ProcessUnits.java
@@ -0,0 +1,22 @@
+public class ProcessUnits {
+	
+	/**
+	 * Kelas Mapper
+	 * urutan Generic di Mapper :
+	 *   LongWritable : tipe input key
+	 *   Text : tipe input value
+	 *   Text : tipe output key
+	 *   IntWritable : tipe output value
+	 */
+	public static class E_EMapper extends MapReduceBase 
+			implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output,
+				Reporter reporter) throws IOException {
+			String line = value.toString();
+
+		}
+
+	}
+
+}
