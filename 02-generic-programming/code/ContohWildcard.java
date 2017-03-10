4a86dd7517f86414656182f3f2851a0d98afd094
tamami <tamami.oka@gmail.com>
2017-03-10 13:31:01 +0700

    finishing kode untuk contoh materi generic

--- 02-generic-programming/code/ContohWildcard.java
+++ 02-generic-programming/code/ContohWildcard.java
@@ -1,9 +1,9 @@
 public class ContohWildcard {
 
 	// percobaan pertama karena tidak menggunakan wildcard
-	public void gambarDonk(ContohKelasGeneric<BentukGeometri> data) {
+	//public void gambarDonk(ContohKelasGeneric<BentukGeometri> data) {
 	// percobaan kedua yang berhasil
-	//public void gambarDonk(ContohKelasGeneric<?> data) {
+	public void gambarDonk(ContohKelasGeneric<? extends BentukGeometri> data) {
 		BentukGeometri geo = (BentukGeometri) data.get();
 		geo.gambar();
 	}
@@ -16,6 +16,13 @@ public class ContohWildcard {
 		Kotak kotak = new Kotak();
 		gen.add(kotak);
 		app.gambarDonk(gen);
+
+		// percobaan ketiga, gagal saat runtime
+		// perbaikannya di method gambarDonk harus dikasih lower bounded
+		ContohKelasGeneric<Integer> gen2 = new ContohKelasGeneric<>();
+
+		//gen2.add(3);
+		//app.gambarDonk(gen2);
 	}
 
 
