66a48df6a86243f1abec5e5444e7e0d42b187bd2
tamami <tamami.oka@gmail.com>
2017-03-10 22:33:20 +0700

    finish contoh RSA asimetri enkripsi

--- 10-cryptography-BELUM-SELESAI/code/Coba08AsymmetricSimple.java
+++ 10-cryptography-BELUM-SELESAI/code/Coba08AsymmetricSimple.java
@@ -3,9 +3,31 @@ import java.security.KeyPair;
 import java.io.File;
 import java.io.ObjectOutputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.security.PublicKey;
+import java.security.PrivateKey;
+import javax.crypto.Cipher;
 
 public class Coba08AsymmetricSimple {
 	public static void main(String args[]) {
+		Coba08AsymmetricSimple app = new Coba08AsymmetricSimple();
+
+		System.out.println("membuat public key dan private key");
+		app.generateKey("private.key", "public.key");
+
+		String message = "Pesan ini harus dirahasiakan, jangan sampai ada yang tau";
+		System.out.println("proses enkripsi...");
+		PublicKey publicKey = app.getPublicKey("public.key");
+		byte[] encrypted = app.encrypt(message, publicKey);
+
+		System.out.println("proses dekripsi...");
+		PrivateKey privateKey = app.getPrivateKey("private.key");
+		String result = app.decrypt(encrypted, privateKey);
+
+		System.out.println("isi pesan : " + message);
+		System.out.println("hasil enkripsi : " + new String(encrypted));
+		System.out.println("hasil dekripsi : " + result);
 	}
 
 	public void generateKey(String privateKeyFile, String publicKeyFile) {
@@ -26,6 +48,54 @@ public class Coba08AsymmetricSimple {
 			ObjectOutputStream oosPriKey = new ObjectOutputStream(new FileOutputStream(privateFile));
 			oosPriKey.writeObject(key.getPrivate());
 			oosPriKey.close();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
+
+	public PublicKey getPublicKey(String keyFile) {
+		PublicKey publicKey = null;
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(keyFile));
+			publicKey = (PublicKey) ois.readObject();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return publicKey;
+	}
+
+	public PrivateKey getPrivateKey(String keyFile) {
+		PrivateKey privateKey = null;
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(keyFile));
+			privateKey = (PrivateKey) ois.readObject();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return privateKey;
+	}
+
+	public byte[] encrypt(String message, PublicKey key) {
+		byte[] cipherText = null;
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			cipherText = cipher.doFinal(message.getBytes());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
+
+	public String decrypt(byte[] encrypted, PrivateKey key) {
+		byte[] decryptedText = null;
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			decryptedText = cipher.doFinal(encrypted);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new String(decryptedText);
 	}
 }
