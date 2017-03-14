d1c4401e86c72110624d4c68b827de57e2c0c409
tamami <tamami.oka@gmail.com>
2017-03-14 13:55:03 +0700

    update penjelasan dan urutan coding untuk generic

--- 02-generic-programming/code/TestKelasGeneric.java
+++ 02-generic-programming/code/TestKelasGeneric.java
@@ -3,5 +3,8 @@ public class TestKelasGeneric {
 		ContohKelasGeneric<Integer> contoh = new ContohKelasGeneric<Integer>();
 		contoh.add(2);
 		System.out.println(contoh.get());
+
+		// error saat compile, harusnya tipe integer
+		//contoh.add("Ini teks");
 	}
 }
