b64999b2909b3155c373ab0928603c4421f88d62
tamami <tamami.oka@gmail.com>
2017-03-22 11:51:03 +0700

    cuma nyoba ubah kode ke unlimit thread untuk akses jconsole

--- 03-multithreading/code/ContohExtendThread.java
+++ 03-multithreading/code/ContohExtendThread.java
@@ -1,10 +1,13 @@
 public class ContohExtendThread extends Thread {
 	public void run() {
-		System.out.println("thread berjalan");
+		while(true) {
+			System.out.println("thread berjalan");
+		}
 	}
 
 	public static void main(String args[]) {
 		ContohExtendThread app = new ContohExtendThread();
+		app.setName("thread tamami");
 		app.start();
 	}
 }
